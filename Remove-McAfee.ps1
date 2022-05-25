c85a4ad076e994030eb4f529e5fdb99eef849e29
Dymin-justinw <106171818+Dymin-justinw@users.noreply.github.com>
2022-05-25 11:04:16 -0500

    Added logic to check for the McAfee process running

--- Remove-McAfee.ps1
+++ Remove-McAfee.ps1
@@ -4,16 +4,29 @@
 $filePath = "C:\temp"
 $fileName = "C:\temp\McAfee_removal.zip"
 $url = "https://github.com/DyminSystems/AV_Removal/raw/main/McAfee_Removal.zip"
-Start-BitsTransfer -source $url -Destination $filepath
-Start-Sleep -s 15
-if (test-path -path $fileName){
-    Write-host "Found McAfee Removal Tool - Expanding archive"
-    Expand-Archive -LiteralPath $fileName -DestinationPath $filePath
-    Write-Host "Expansion Complete - Attempting to uninstall McAfee..."
+$mcafeeInstalled = Get-Process | where {$_.ProcessName -Like "*mcafee*"} -ErrorAction SilentlyContinue
+if ($mcafeeInstalled -ne $null)
+{
+    Write-host "McAfee process found beginning uninstallation..."
+    Start-BitsTransfer -source $url -Destination $filepath
     Start-Sleep -s 15
-    Set-Location "c:\temp\McAfee_Removal"
-    .\Mccleanup.exe -p StopServices,MFSY,PEF,MXD,CSP,Sustainability,MOCP,MFP,APPSTATS,Auth,EMproxy,FWdiver,HW,MAS,MAT,MBK,MCPR,McProxy,McSvcHost,VUL,MHN,MNA,MOBK,MPFP,MPFPCU,MPS,SHRED,MPSCU,MQC,MQCCU,MSAD,MSHR,MSK,MSKCU,MWL,NMC,RedirSvc,VS,REMEDIATION,MSC,YAP,TRUEKEY,LAM,PCB,Symlink,SafeConnect,MGS,WMIRemover,RESIDUE -v -s
+    if (test-path -path $fileName){
+        Write-host "Found McAfee Removal Tool - Expanding archive"
+        Expand-Archive -LiteralPath $fileName -DestinationPath $filePath
+        Write-Host "Expansion Complete - Attempting to uninstall McAfee..."
+        Start-Sleep -s 15
+        Set-Location "c:\temp\McAfee_Removal"
+        .\Mccleanup.exe -p StopServices,MFSY,PEF,MXD,CSP,Sustainability,MOCP,MFP,APPSTATS,Auth,EMproxy,FWdiver,HW,MAS,MAT,MBK,MCPR,McProxy,McSvcHost,VUL,MHN,MNA,MOBK,MPFP,MPFPCU,MPS,SHRED,MPSCU,MQC,MQCCU,MSAD,MSHR,MSK,MSKCU,MWL,NMC,RedirSvc,VS,REMEDIATION,MSC,YAP,TRUEKEY,LAM,PCB,Symlink,SafeConnect,MGS,WMIRemover,RESIDUE -v -s
+        }
+    else{
+        Write-Host "McAfee Removal Tool not found - $($filepath)"
+        Write-Host "Exiting..."
+    }
 }
-else{
-    Write-Host "McAfee Removal Tool not found - $($filepath)"
+else
+{
+    write-host "McAfee Process not found"
+    write-host "Exiting..."
+    Exit
 }
+
