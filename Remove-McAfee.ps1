3a81531b99b687ede7ee927818bb45977dbccc1e
Dymin-justinw <106171818+Dymin-justinw@users.noreply.github.com>
2022-05-25 11:18:35 -0500

    Set sleep timers to 5 seconds

--- Remove-McAfee.ps1
+++ Remove-McAfee.ps1
@@ -6,12 +6,12 @@ $fileName = "C:\temp\McAfee_removal.zip"
 $url = "https://github.com/DyminSystems/AV_Removal/raw/main/McAfee_Removal.zip"
 
     Start-BitsTransfer -source $url -Destination $filepath
-    Start-Sleep -s 15
+    Start-Sleep -s 5
     if (test-path -path $fileName){
         Write-host "Found McAfee Removal Tool - Expanding archive"
         Expand-Archive -LiteralPath $fileName -DestinationPath $filePath
         Write-Host "Expansion Complete - Attempting to uninstall McAfee..."
-        Start-Sleep -s 15
+        Start-Sleep -s 5
         Set-Location "c:\temp\McAfee_Removal"
         .\Mccleanup.exe -p StopServices,MFSY,PEF,MXD,CSP,Sustainability,MOCP,MFP,APPSTATS,Auth,EMproxy,FWdiver,HW,MAS,MAT,MBK,MCPR,McProxy,McSvcHost,VUL,MHN,MNA,MOBK,MPFP,MPFPCU,MPS,SHRED,MPSCU,MQC,MQCCU,MSAD,MSHR,MSK,MSKCU,MWL,NMC,RedirSvc,VS,REMEDIATION,MSC,YAP,TRUEKEY,LAM,PCB,Symlink,SafeConnect,MGS,WMIRemover,RESIDUE -v -s
         }
