efadc178e29ec4ff2b0236106a1a8c1d731c6cf3
Dymin-justinw <106171818+Dymin-justinw@users.noreply.github.com>
2022-05-26 11:23:07 -0500

    Added error handling logic to script

--- Remove-Bitdefender.ps1
+++ Remove-Bitdefender.ps1
@@ -6,16 +6,36 @@ $filePath = "C:\temp"
 $fileName = "C:\temp\BEST_uninstallTool.exe"
 $url = "https://download.bitdefender.com/SMB/Hydra/release/bst_win/uninstallTool/BEST_uninstallTool.exe"
 
-Start-BitsTransfer -source $url -Destination $filepath
+try {
+    Start-BitsTransfer -source $url -Destination $filepath
+}
+catch {
+    Write-Host "Unable to start file download, check permissions on $($filePath) or URL"
+}
+
 Start-Sleep -s 5
 if (test-path -path $fileName){
     Write-host "Found $($product) Removal Tool - Expanding archive"
-    Expand-Archive -LiteralPath $fileName -DestinationPath $filePath
-    Write-Host "Expansion Complete - Attempting to uninstall $($product)..."
-    Start-Sleep -s 5
-    Set-Location "c:\temp"
-    .\BEST_uninstallTool.exe
+    try {
+        Expand-Archive -LiteralPath $fileName -DestinationPath $filePath
+        Write-Host "Expansion Complete - Attempting to uninstall $($product)..."
+        Start-Sleep -s 5
+        Set-Location "c:\temp"
+        try {
+            .\BEST_uninstallTool.exe /nowait
+        }
+        catch {
+            Write-Host "Uninstallation failed"
+            Write-Host "Exiting..."
+        }
         
+    }
+    catch {
+        Write-host "Failed to expand archive"
+        Write-host "Exiting..."
+        Exit
+    }
+
 }
 else{
     Write-Host "$($product) Removal Tool not found - $($filepath)"
