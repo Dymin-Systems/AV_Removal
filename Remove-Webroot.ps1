39cd967c3da7aed832210709c352d449bf68c2a2
Dymin-justinw <106171818+Dymin-justinw@users.noreply.github.com>
2022-07-05 14:27:13 -0500

    Added check for 64 bit or 32bit webroot install

--- Remove-Webroot.ps1
+++ Remove-Webroot.ps1
@@ -1,15 +1,41 @@
-# This script is designed to uninstall Webroot from computers which have failed to uninstall with the standard script
-#Set-location "C:\Program Files (x86)\Webroot"
-#.\WRSA.exe -uninstall
-$Status = Get-Process notepad -ErrorAction SilentlyContinue
-while ($Status -ne $null)
-{
-    Write-host "Webroot uninstallation in progress..."
-    Start-Sleep -S 30
-    $Status = Get-Process notepad -ErrorAction SilentlyContinue
+## Check for the Webroot install directory and if found attempts to uninstall
+Write-Host "Checking for Webroot..."
+if (Test-Path -path "C:\Program Files (x86)\Webroot\"){
+    Write-Host "Found Webroot - Attempting uninstall"
+    Start-Process -FilePath "C:\Program Files (x86)\Webroot\wrsa.exe"  -ArgumentList "-uninstall"
+    Start-Sleep -s 30
+    try{
+        Get-process -name "WRSA"
+        Stop-process -name "WRSA"
+        Remove-Item "c:\program files (x86)\Webroot\" -Recurse -Force
+    }
+    Catch{
+        Try{
+            Remove-Item "c:\program files (x86)\Webroot\" -Recurse -Force
+        }
+        catch{
+            Exit
+        }
+    }
 }
-else
-{
-    write-host "Webroot uninstallation complete."
-    Exit
+elseif(Test-Path -path "C:\Program Files\Webroot\"){
+    Write-Host "Found Webroot - Attempting uninstall"
+    Start-Process -FilePath "C:\Program Files\Webroot\wrsa.exe"  -ArgumentList "-uninstall"
+    Start-Sleep -s 30
+    try{
+        Get-process -name "WRSA"
+        Stop-process -name "WRSA"
+        Remove-Item "c:\program files\Webroot\" -Recurse -Force
+    }
+    Catch{
+        Try{
+            Remove-Item "c:\program files\Webroot\" -Recurse -Force
+        }
+        catch{
+            Exit
+        }
+    }
+}
+else{
+    Write-Host "Webroot not found - Exiting"
 }
