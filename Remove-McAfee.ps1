3438a6ad047665641c7c895caa0b140e083e2904
Justin Wilson <106171818+Dymin-justinw@users.noreply.github.com>
2023-07-07 14:38:15 -0500

    Update Remove-McAfee.ps1

--- Remove-McAfee.ps1
+++ Remove-McAfee.ps1
@@ -3,7 +3,7 @@
 
 $filePath = "C:\temp"
 $fileName = "C:\temp\McAfee_removal.zip"
-$url = "https://github.com/DyminSystems/AV_Removal/raw/main/McAfee_Removal.zip"
+$url = "https://github.com/Dymin-Systems/AV_Removal/raw/main/McAfee_Removal.zip"
 
 Start-BitsTransfer -source $url -Destination $filepath
 Start-Sleep -s 5
