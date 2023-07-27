f5eae71b98be2fb5a7e6b63d85599961a22fd815
Justin Wilson <106171818+Dymin-justinw@users.noreply.github.com>
2023-07-27 10:09:21 -0500

    Update Remove-McAfee.ps1
    Commented out write-host commands

--- Remove-McAfee.ps1
+++ Remove-McAfee.ps1
@@ -8,15 +8,15 @@ $url = "https://github.com/Dymin-Systems/AV_Removal/raw/main/McAfee_Removal.zip"
 Start-BitsTransfer -source $url -Destination $filepath
 Start-Sleep -s 5
 if (test-path -path $fileName){
-    Write-host "Found McAfee Removal Tool - Expanding archive"
+    #Write-host "Found McAfee Removal Tool - Expanding archive"
     Expand-Archive -LiteralPath $fileName -DestinationPath $filePath
-    Write-Host "Expansion Complete - Attempting to uninstall McAfee..."
+    #Write-Host "Expansion Complete - Attempting to uninstall McAfee..."
     Start-Sleep -s 5
     Set-Location "c:\temp\McAfee_Removal"
     .\Mccleanup.exe -p StopServices,MFSY,PEF,MXD,CSP,Sustainability,MOCP,MFP,APPSTATS,Auth,EMproxy,FWdiver,HW,MAS,MAT,MBK,MCPR,McProxy,McSvcHost,VUL,MHN,MNA,MOBK,MPFP,MPFPCU,MPS,SHRED,MPSCU,MQC,MQCCU,MSAD,MSHR,MSK,MSKCU,MWL,NMC,RedirSvc,VS,REMEDIATION,MSC,YAP,TRUEKEY,LAM,PCB,Symlink,SafeConnect,MGS,WMIRemover,RESIDUE -v -s
         
 }
 else{
-    Write-Host "McAfee Removal Tool not found - $($filepath)"
-    Write-Host "Exiting..."
+    #Write-Host "McAfee Removal Tool not found - $($filepath)"
+    #Write-Host "Exiting..."
 }
