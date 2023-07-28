e349e64d8d4eb3a4985872e8302f57fc8b260f68
Justin Wilson <106171818+Dymin-justinw@users.noreply.github.com>
2023-07-28 11:49:15 -0500

    Removed write-hosts

--- Remove-Bitdefender.ps1
+++ Remove-Bitdefender.ps1
@@ -9,51 +9,34 @@ $url = "https://download.bitdefender.com/SMB/Hydra/release/bst_win/uninstallTool
 try {
     #check if the directory exists
     if (!(test-path -path $filePath)){
-        #Write-Host "Creating $($filePath)"
         New-Item -ItemType Directory -Path $filePath
     }
 
     Start-BitsTransfer -source $url -Destination $filepath
 }
-catch {
-    #Write-Host "Unable to start file download, check permissions on $($filePath) or URL"
-}
+catch {}
 
 Start-Sleep -s 5
 if (test-path -path $fileName){
-    #Write-host "Found $($product) Removal Tool - Expanding archive"
     try {
         Expand-Archive -LiteralPath $fileName -DestinationPath $filePath
-        #Write-Host "Expansion Complete - Attempting to uninstall $($product)..."
         Start-Sleep -s 2
         
         #Run the uninstaller
         try {
             Start-Process -FilePath $filename
-            #Write-Host "Uninstallation Complete"
-            #Write-Host "Exiting..."
         }
-        catch {
-            #Write-Host "Uninstallation failed"
-            #Write-Host "Exiting..."
-        }
+        catch {}
         #cleanup the files
         try {
             Remove-Item -Path $filePath -Force -Recurse
         }
-        catch{
-
-        }
+        catch{}
         
     }
     catch {
-        #Write-host "Failed to expand archive"
-        #Write-host "Exiting..."
         Exit
     }
 
 }
-else{
-    #Write-Host "$($product) Removal Tool not found - $($filepath)"
-    #Write-Host "Exiting..."
-}
+else{}
